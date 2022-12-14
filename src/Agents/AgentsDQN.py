7962c7d86b782ffdd5162231bb87a7f39846e3c1
LukasSteffensen <lste@itu.dk>
2022-12-14 13:12:25 +0100

    run_final_test

--- src/Agents/AgentsDQN.py
+++ src/Agents/AgentsDQN.py
@@ -152,6 +152,25 @@ class AgentDQN(AgentBase):
         self.memory = deque([], maxlen=self.N)
         print("model loaded")
 
+    def run_final_test(self, config, episodes_per_test=100, tics_per_action=12, epochs=10):
+        self.set_up_game_environment(config, False)
+
+        self.load_model()
+
+        writer = SummaryWriter(comment=('_' + self.model_name + '_final_test'))
+        writer.filename_suffix = self.model_name
+        first_run = False
+
+        for epoch in range(epochs):
+            avg_score = 0.0
+
+            for e in trange(episodes_per_test):
+                avg_score += self.test_run_fast(tics_per_action)
+
+            avg_score /= episodes_per_test
+            writer.add_scalar('Score_epoch_size_' + str(episodes_per_test), avg_score, epoch)
+
+
     def start_training(self, config, epoch_count=10, episodes_per_epoch=100, episodes_per_test=10, tics_per_action=12, hardcoded_path=False,
                        fast_train=True, tune_config=None):
         if tics_per_action < self.frame_stack_size:
